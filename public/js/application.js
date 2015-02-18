30dcc4883b2692aaa72121d349e659914e3f49dc
James Parseghian <jjparseghian@gmail.com>
2015-02-18 18:11:03 -0500

    Implement ajax for photos in a dive log

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,65 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('.show_btn').on('click',function(event){
+    event.preventDefault();
+    var $target = $(event.target);
+    var url = $target.attr('href');
+    $.ajax({
+      type: 'get',
+      url: url,
+      // dataType: "json", --- don't need this for get
+      success: function(response){
+        console.log("success" , response);
+        $('#show_photos').html(response);
+        $('#show_photos').toggle();
+      },
+      error: function(response){
+        console.log("error" , response);
+      }
+    })
+  });
+
+  $('.add_btn').on('click',function(event){
+    event.preventDefault();
+    var $target = $(event.target);
+    var url = $target.attr('href');
+    $.ajax({
+      type: 'get',
+      url: url,
+      // dataType: "json", --- don't need this for get
+      success: function(response){
+        console.log("success" , response);
+        $('#add_photos').html(response);
+        $('#add_photos').toggle();
+      },
+      error: function(response){
+        console.log("error" , response);
+      }
+    })
+  });
+
+  $('#add_photos').on('submit', '.new_photo_form', function(event){
+    event.preventDefault();
+    var $form = $(event.target);
+    console.log($form);
+    var dataToSend = $form.serialize();
+    console.log(dataToSend);
+    var type = $form.attr('method');
+    console.log(type);
+    var url = $form.attr('action');
+    console.log(url);
+    $.ajax({
+      type: type,
+      url: url,
+      data: dataToSend,
+      // dataType: "json",
+      success: function(response){
+        console.log("success ", response);
+      },
+      error: function(response){
+        console.log("error ", response);
+      }
+    })
+  });
+
 });
